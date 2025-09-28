5f6a52c922e38168022568540abdac649a1bc1f7
Malik Robinson <malik16.mr@gmail.com>
2025-09-27 21:10:09 -0400

    Version one of PasteTree correct masonry stying

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -69,7 +69,6 @@ function createPost(imageSrc, userName, randomtime, id, sizeClass= "") {
               <path d="M13.91,6.75c-1.17,2.25-4.3,5.31-6.07,6.94c-0.1903,0.1718-0.4797,0.1718-0.67,0C5.39,12.06,2.26,9,1.09,6.75&#xA;&#x9;C-1.48,1.8,5-1.5,7.5,3.45C10-1.5,16.48,1.8,13.91,6.75z"/>
               </svg>
               <div class="number-likes" id="number-likes" >0</div>
-              <span class="sr-only">Like</span>
             </button>
 
             <button class="comment-button" id="comment-button">
@@ -82,12 +81,10 @@ function createPost(imageSrc, userName, randomtime, id, sizeClass= "") {
               </g>
               </svg>
               <div class="number-comments" id="number-comments" >0</div>
-              <span class="sr-only">Comment</span>
             </button>
 
             <button class="share-button" id="share-button">
               <svg width="50px" height="50px" viewBox="0 0 24 24" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg"><path d="M21.707,11.293l-8-8A.99991.99991,0,0,0,12,4V7.54492A11.01525,11.01525,0,0,0,2,18.5V20a1,1,0,0,0,1.78418.62061,11.45625,11.45625,0,0,1,7.88672-4.04932c.0498-.00635.1748-.01611.3291-.02588V20a.99991.99991,0,0,0,1.707.707l8-8A.99962.99962,0,0,0,21.707,11.293ZM14,17.58594V15.5a.99974.99974,0,0,0-1-1c-.25488,0-1.2959.04932-1.56152.085A14.00507,14.00507,0,0,0,4.05176,17.5332,9.01266,9.01266,0,0,1,13,9.5a.99974.99974,0,0,0,1-1V6.41406L19.58594,12Z"/></svg>
-              <span class="sr-only">Share</span>
             </button>
           </div>
         </div>
@@ -142,6 +139,21 @@ function addPostEventListeners() {
   });
 }
 
+function resizeGridItem(item) {
+  const grid = document.getElementById("main");
+  const rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  const rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('gap'));
+  const contentHeight = item.querySelector('.post-wrapper').offsetHeight + item.querySelector('.post-image').offsetHeight;
+  const rowSpan = Math.ceil((contentHeight + rowGap) / (rowHeight + rowGap));
+  item.style.gridRowEnd = `span ${rowSpan}`;
+}
+
+function resizeAllGridItems() {
+  document.querySelectorAll(".post-item").forEach(item => resizeGridItem(item));
+}
+
+window.addEventListener("resize", resizeAllGridItems); // 🔥 Recalculate on layout shift
+
 function renderPosts() {
   const main = document.getElementById("main");
   main.innerHTML = "";
@@ -161,9 +173,24 @@ function renderPosts() {
 
     // Build post
     main.innerHTML += createPost(img, userName, time, index, sizeClass);
+
+    const postItem = main.lastElementChild;
+    const image = postItem.querySelector("img");
+
+    image.onload = () => {
+    resizeGridItem(item);
+    };
+
   });
 
   addPostEventListeners();
+
+  // Ensure images resize properly
+  const allItems = document.querySelectorAll(".post-item");
+  allItems.forEach(item => {
+    const image = item.querySelector("img");
+    image.onload = () => resizeGridItem(item);
+  });
 };
 
 function GetHtmlStructure(randomname) {
